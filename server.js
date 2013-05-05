9e94d10e1c43153ecec906b293ae571b3f95c643
Kirill Maksimov <k.maksimov@applicaster.com>
2013-05-06 00:10:53 +0300

    Utils

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 load('vertx.js');
 load('./lib/contrib/underscore.js');
+load('./lib/utils.js');
 
 var router = new vertx.RouteMatcher();
 var get = router.get;
 
-var render = function(req, filename, attributes) {
-    vertx.fileSystem.readFile(filename, function(err, res) {
-        if (!err) {
-            var source = res;
-            var html = _.template(source.toString())(attributes);
-            return req.response.end(html);
-        }
-    });
-};
 
 get("/", function(req) {
     render(req, "public/index.html", {
