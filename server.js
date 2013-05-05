68c12405d8e57f0c12a4664bcea836f36bda922d
Kirill Maksimov <k.maksimov@applicaster.com>
2013-05-05 23:45:46 +0300

    Underscore!

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 load('vertx.js');
+load('./lib/contrib/underscore.js');
 
 var router = new vertx.RouteMatcher();
 var get = router.get;
 
+var render = function(req, filename, attributes) {
+    vertx.fileSystem.readFile(filename, function(err, res) {
+        if (!err) {
+            var source = res;
+            var html = _.template(source.toString())(attributes);
+            return req.response.end(html);
+        }
+    });
+};
+
 get("/", function(req) {
-    req.response.sendFile("public/index.html");
+    render(req, "public/index.html", {
+        cat: "Azshara", feels: "very much"
+    });
 });
 
 vertx.createHttpServer().requestHandler(router).listen(8080, "0.0.0.0");
