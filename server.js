77c53484477080c6c1d7ab5d12582b217c6dd897
Kirill Maksimov <k.maksimov@applicaster.com>
2013-05-24 17:34:29 +0300

    Simplify dsl

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 load('vertx.js');
 load('./lib/contrib/underscore.js');
-load('./lib/utils.js');
 
 var router = new vertx.RouteMatcher();
-var get = router.get;
 
-
-get("/", function(req) {
-    render(req, "public/index.html", {
-        cat: "Azshara", feels: "very much"
+var get = function (path, callback) {
+    return router.get(path, function (req) {
+        return req.response.end(callback(req));
     });
+};
+
+var template = _.memoize(function (filename) {
+    var source = vertx.fileSystem.readFileSync(filename).toString();
+    return _.template(source);
+});
+
+get("/", function (req) {
+    var attributes = {cat: "Azshara", feels: "very much"};
+    return template("public/index.html")(attributes);
 });
 
 vertx.createHttpServer().requestHandler(router).listen(8080, "0.0.0.0");
